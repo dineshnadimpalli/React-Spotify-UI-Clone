dcdc20012b06292860eaf08fc96ff9244895896b
Dinesh Nadimpalli <dinesh601048@gmail.com>
2021-05-12 13:48:05 +0530

    Modified the initial setup of store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,5 @@
 export const initialState = {
     // token: null,
-    // token: 'BQAvpKSaFbROrxEmPa_oj0n_9RnO6kWBaWDKS9Z4M4ntlUO9LTlp7dMMHaru3xEl4uWtzOiCA8oMHW6_T0_r7QJPQCfIXxCDKHoDH2Jq3bcfjK3bFVnr1ER5apC6e_2onvwhfPS8rGLh_PR_loXI4EpNQTIGSA0kMKWqY_hcsAm-kBSA5ACF',
     user: null,
     playlists: [],
     playing: false,
